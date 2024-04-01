e4d5256b7d6b1ce8f35260f222edf6a26c46e57d
Saki <cotacminnus@gmail.com>
2024-04-01 11:46:03 -0500

    resources

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import os
 import discord
 
-token = os.environ['TOKEN']
+intents = discord.Intents.default()
+intents.message_content = True
+
+Sakiko = discord.Sakiko(intents=intents)
+
+@Sakiko.event
+async def on_ready():
+    print(f'We have logged in as {Sakiko.user}')
+
+@Sakiko.event
+async def on_message(message):
+    if message.author == Sakiko.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+Sakiko.run(os.getenv("TOKEN"))
